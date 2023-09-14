f5641081474041f77418617a6ad6247d564fd2b8
Ann Samuel <annsamuel88@gmail.com>
2023-09-14 16:44:07 -0500

    base framework

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,25 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@RunWith(Cucumber.class)
+	@CucumberOptions(plugin = {"pretty", "html:target/Report"},
+	monochrome = false,
+	tags= "@tag",
+	features= {"src/test/resources/features"},
+	glue={"stepdefinition"}
+	)
+	
+	
+	public class TestRunner  extends AbstractTestNGCucumberTests{
+		@Override
+		@DataProvider(parallel = false)
+		public Object[][] scenarios(){
+			return super.scenarios();
+		}
+	}
