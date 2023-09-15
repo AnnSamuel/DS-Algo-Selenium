3114a8adaf57c3a4c84f2189993d34c729e84baf
Ann Samuel <annsamuel88@gmail.com>
2023-09-15 16:32:22 -0500

    added utilities package

--- src/main/java/DsAlgo_pom/Home.java
+++ src/main/java/DsAlgo_pom/Home.java
@@ -1,5 +1,24 @@
 package DsAlgo_pom;
 
-public class Home {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Home {
+	public static WebDriver driver = new ChromeDriver();
+	String URL = "https://dsportalapp.herokuapp.com/";
+	By getStrtedBtn = By.xpath("//button[text()='Get Started']");
+
+	public void dsAlgo() {
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get(URL);
+	}
+	
+	public void homePg()
+	{
+		driver.findElement(getStrtedBtn).click();
+	}
 }
